f85bc5bb51c625d53088dee1d22918fe3a28ee63
Faysal-MD <faysalmahmud735@gmail.com>
2025-04-29 22:51:09 +0600

    learned: siblings with xpath travarse

---
+++ src/Locators3.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Locators3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "/Java/chromedriver-win64/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+//		Sibling - child to parent traverse
+		//header/div/button[1]/following-sibling::button[1]
+//		Parent to child = //header/div/button[1]
+//		Sibling to Sibling = /following-sibling::button[1]
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		driver.findElement(By.xpath("//header/div/button[1]/following-sibling::button[1]")).click();
+		
+	}
+
+}
